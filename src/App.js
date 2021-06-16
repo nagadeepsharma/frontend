fbe6881c6c92d942857b3c8368ca50c03b36c68e
nagadeepsharma <nagadeep.chandragiri@gmail.com>
2021-06-16 22:46:28 +0530

    saved

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import './App.css';
 import {useContext} from 'react'
-import Home1 from './components/Home1';
-import Home2 from './components/Home2';
-import Home3 from './components/Home3';
-import Home4 from './components/Home4';
-import Home5 from './components/Home5';
-import Home6 from './components/Home6';
+import Home from './components/Home';
 import { GlobalStyle } from './components/GlobalStyle';
 import { themecontext } from './components/themecontext';
 function App() {
@@ -13,12 +8,7 @@ function App() {
   return (
     <div className="App">
       <GlobalStyle colors={colors} />
-      <Home1 />
-      <Home2 />
-      <Home3 />
-      <Home4 />
-      <Home5 />
-      <Home6 />
+      <Home />
     </div>
   );
 }
